4b67f2679cc217792e84b9c1099cc68687545863
microlws <microlws@gmail.com>
2018-06-19 20:20:16 +0200

    why wont it work

--- src/App.js
+++ src/App.js
@@ -5,16 +5,26 @@ class App extends Component {
   constructor(props){
     super(props)
       this.state = {
-        board: Array(9).fill("X")
+        tiles: Array(9).fill(null),
     }
   }
+  handleClick(index){
+    let newTiles = this.state.tiles
+    newTiles[index] = "X"
+    this.setState({
+      tiles: newTiles
+    })
+    console.log(this.state.tiles)
+    //console.log(index)
+  }
+
   render() {
-    const Box = this.state.board.map(box => <div className='Main__Board-tiles'>{Box}</div> )
+    const Tiles = this.state.tiles.map((box, index) => <div className='Main__Board-tiles' key={index} onClick={() => this.handleClick(index)}>{Tiles}</div> )
       return (
         <div className='Main'>
            <h1 className='Main__Title'>Tic Tac Toe App</h1>
           <div className='Main__Board'>
-             {Box}      
+             {Tiles}      
           </div>
         </div>   
     );
