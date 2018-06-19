5c64587c5e11cd03a4fac8c4f5b4ca35758fc248
microlws <microlws@gmail.com>
2018-06-19 14:00:20 +0200

    Need help

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+      this.state = {
+        board: Array(9).fill("X")
+    }
+  }
   render() {
-    return (
-      <div className='Main'>
-         <h1 className='Main__Title'>Tic Tac Toe App</h1>
-         <div className='Main__Board'>
-            <div className='Main__Board-tiles'>X</div>
-            <div className='Main__Board-tiles'>X</div>
-            <div className='Main__Board-tiles'>X</div>
-            <div className='Main__Board-tiles'>X</div>
-            <div className='Main__Board-tiles'>X</div>
-            <div className='Main__Board-tiles'>X</div>
-            <div className='Main__Board-tiles'>X</div>
-            <div className='Main__Board-tiles'>X</div>
-            <div className='Main__Board-tiles'>X</div>
-         </div>
-      </div>   
+    const Box = this.state.board.map(box => <div className='Main__Board-tiles'>{Box}</div> )
+      return (
+        <div className='Main'>
+           <h1 className='Main__Title'>Tic Tac Toe App</h1>
+          <div className='Main__Board'>
+             {Box}      
+          </div>
+        </div>   
     );
   }
 }
